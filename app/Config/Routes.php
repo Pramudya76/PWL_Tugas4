063816f8506df28355d0bc6c68774eb07ef20b0c
Pramudya76 <pramudyarama76@gmail.com>
2025-03-19 19:22:32 +0700

    first commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('hello/(:any)', 'Home::hello/$1');
+$routes->get('kategori', 'Home::kategori');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,17 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('front');
+        return view('welcome_message');
     }
+
+    public function hello($nama=null) {
+        $data['nama'] = $nama;
+        $data['judul'] = 'Judul halaman';
+        return view('front', $data);
+    }
+
+    public function kategori() {
+        return view('kategori');
+    }
+
 }
