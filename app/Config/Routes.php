4438106f9070e8cd2c08e4208bbc319c9f881b41
Pramudya76 <pramudyarama76@gmail.com>
2025-04-16 17:29:36 +0700

    pertemuan 7 filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,14 +5,17 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('hello/(:any)', 'Home::hello/$1');
-$routes->get('kategori', 'Home::kategori');
-$routes->get('alatTulis', 'Home::alatTulis');
-$routes->get('pakaian', 'Home::pakaian');
-$routes->get('snack', 'Home::snack');
-$routes->get('/produk', 'Home::produk');
-$routes->get('/keranjang', 'Home::keranjang');
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+// $routes->get('hello/(:any)', 'Home::hello/$1');
+// $routes->get('kategori', 'Home::kategori');
+// $routes->get('alatTulis', 'Home::alatTulis');
+// $routes->get('pakaian', 'Home::pakaian');
+// $routes->get('snack', 'Home::snack');
+// $routes->get('/produk', 'Home::produk');
+// $routes->get('/keranjang', 'Home::keranjang');
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
+
+$routes->get('produk', 'Home::produk', ['filter' => 'auth']);
+$routes->get('keranjang', 'Home::keranjang', ['filter' => 'auth']);
